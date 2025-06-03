54957b89d500d86efdfbd923ff4566092c99cfdf
Sadam <sadebola@myseneca.ca>
2025-06-03 14:23:35 -0400

    Initial commit for Lab 4

---
+++ test_lab4b.py
@@ -0,0 +1,8 @@
+import lab4b
+
+list1 = [1, 2, 3, 4, 5]
+list2 = [2, 1, 0, -1, -2]
+
+print(lab4b.join_lists(list1, list2))
+print(lab4b.match_lists(list1, list2))
+print(lab4b.diff_lists(list1, list2))
